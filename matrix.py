64f5098ca8fe7709a4d2ef05045a1868886da09d
GulsherThePython <hassanrazaali@outlook.com>
2026-07-09 12:50:09 -0700

    Added multiplying matrix function to matrix.oy

--- matrix.py
+++ matrix.py
@@ -1,14 +1,10 @@
-import math
+def multiply_matrix_to_vector(m, v):
+    a = m[0]
+    b = m[1]
+    c = m[2]
+    d = m[3]
 
-def multiply_matrice_to_vector(m, v, rows, m_columns):
-    mtx = [
-        [2, 3],
-        [4, 5]
-    ]
+    x = v[0]
+    y = v[1]
 
-    v = [
-        7,
-        8
-    ]
-
-    return 
+    return [a*x + b*y, c*x + d*y]
