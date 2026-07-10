9e78798fb8e18f4b8fa6ae3f9dcc7629d887f5e5
GulsherThePython <HassanRazaAli@outlook.com>
2026-07-10 15:10:26 -0700

    Finished cube function in shapes.py

--- shapes.py
+++ shapes.py
@@ -1,3 +1,12 @@
 def cube(centerx, centery, centerz, side_length):
     half = side_length / 2
-    v1 = (
+    v1 = (centerx - half, centery - half, centerz - half)
+    v2 = (centerx - half, centery - half, centerz + half)
+    v3 = (centerx - half, centery + half, centerz - half)
+    v4 = (centerx - half, centery + half, centerz + half)
+    v5 = (centerx + half, centery - half, centerz - half)
+    v6 = (centerx + half, centery - half, centerz + half)
+    v7 = (centerx + half, centery + half, centerz - half)
+    v8 = (centerx + half, centery + half, centerz + half)
+
+    return v1, v2, v3, v4, v5, v6, v7, v8
