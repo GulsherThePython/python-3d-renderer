701815212ee0889f6e6713e0fd4ccfc3fb5a7f92
GulsherThePython <hassanrazaali@outlook.com>
2026-07-10 11:53:53 -0700

    Fixed camera offset issue

--- projection.py
+++ projection.py
@@ -2,10 +2,12 @@ from math import tan, radians
 
 def project_points(point, fov, window_width):
     x, y, z = point
+    if z <= 0:
+        return None
 
     near = window_width / tan(radians(fov) / 2)
 
-    x_proj = (near * x) / (near + z)
-    y_proj = -(near * y) / (near + z)
+    x_proj = (near * x) / z
+    y_proj = -(near * y) / z
 
     return (x_proj, y_proj)
