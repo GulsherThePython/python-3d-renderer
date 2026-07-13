e3511ef14701bba4f9fb69ae1e64f6a22a48414b
GulsherThePython <HassanRazaAli@outlook.com>
2026-07-13 13:35:39 -0700

    Projected cube verticies in draw.py

--- draw.py
+++ draw.py
@@ -1,6 +1,12 @@
 from projection import project_points
 from matrix import get_index_of_flat_matrix
+from shapes import cube_edges
 import pygame
 
 def draw_cube(screen, vertices, fov, window_width):
     projected_vertices = []
+    for vertex in vertices:
+        projected_vertices.append(project_points((vertex[0], vertex[1], vertex[2]), fov, window_width))
+
+    edges = cube_edges()
+    
