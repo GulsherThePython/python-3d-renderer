319e4601a7325341548696240e65a4076594b968
GulsherThePython <hassanrazaali@outlook.com>
2026-07-27 11:12:17 -0700

    Implemented a YAW system to make camera movement local to camera rotation

--- camera.py
+++ camera.py
@@ -1,17 +1,24 @@
 import pygame
+from math import cos, radians, sin
 
 def handle_camera_movement(camera):
     camera_position = camera["position"]
+    yaw = radians(camera["rotation"][1])
+    move_step = 1
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
-        camera_position[2] -= 1
+        camera_position[0] += sin(yaw) * move_step
+        camera_position[2] -= cos(yaw) * move_step
     if keys[pygame.K_s]:
-        camera_position[2] += 1
+        camera_position[0] -= sin(yaw) * move_step
+        camera_position[2] += cos(yaw) * move_step
     if keys[pygame.K_a]:
-        camera_position[0] += 1
+        camera_position[0] += cos(yaw) * move_step
+        camera_position[2] += sin(yaw) * move_step
     if keys[pygame.K_d]:
-        camera_position[0] -= 1
+        camera_position[0] -= cos(yaw) * move_step
+        camera_position[2] -= sin(yaw) * move_step
     if keys[pygame.K_q]:
         camera_position[1] -= 1
     elif keys[pygame.K_e]:
